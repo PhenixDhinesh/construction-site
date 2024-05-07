e2dbfed6065dbd1e199b5c038daf0d4a346f6c3a
astrostardev <astrostardev@gmail.com>
2024-05-07 22:30:35 +0530

    .

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState, useRef } from "react";
 import menu from "../assets/menu.svg";
 import { FaAngleRight } from "react-icons/fa6";
 import Box from "@mui/material/Box";
@@ -8,125 +8,227 @@ import ListItem from "@mui/material/ListItem";
 import { LiaAngleDownSolid, LiaAngleUpSolid } from "react-icons/lia";
 
 import logo from "../assets/Logo.svg";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Header() {
+  const [showBox, setShowBox] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [showServicesDropdown, setShowServicesDropdown] = useState(false);
+  const drawerRef = useRef(null);
 
-    const [showBox, setShowBox] = useState(false);
-    const [open, setOpen] = useState(false);
-    const [showServicesDropdown, setShowServicesDropdown] = useState(false);
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+  const toggleServicesDropdown = (e) => {
+    e.stopPropagation();
+    setShowServicesDropdown(!showServicesDropdown);
+  };
   
-    const toggleDrawer = (newOpen) => () => {
-      setOpen(newOpen);
-    };
-    const toggleServicesDropdown = (e) => {
-      e.stopPropagation();
-      setShowServicesDropdown(!showServicesDropdown);
-    };
-  
-    const handleMouseEnter = () => {
-      setShowBox(true);
-    };
-  
-    const handleMouseLeave = () => {
+
+  const timerRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+    setShowBox(true);
+  };
+
+  const handleMouseLeave = () => {
+    timerRef.current = setTimeout(() => {
       setShowBox(false);
-    };
-  
-  
-      const drawerList = (
-        <Box sx={{ width: 300 }} role="presentation">
-          <div className="offcanvas_container">
-            <List>
-              <h2>Menu</h2>
-              <div className="offcanvas_lists">
-              <Link to="/" style={{textDecoration:"none",color:"#000"}}><ListItem onClick={toggleDrawer(false)}>Home</ListItem></Link>
-              <ListItem onClick={toggleServicesDropdown} className="off_service">
-                <p>Our Service</p>
-              
-                {showServicesDropdown ? (
+    }, 200); // Adjust delay as needed
+  };
+
+  const drawerList = (
+    <Box sx={{ width: 300 }} role="presentation">
+      <div className="offcanvas_container">
+        <List>
+          <h2>Menu</h2>
+          <div className="offcanvas_lists">
+            <Link to="/" style={{ textDecoration: "none", color: "#000" }}>
+              <ListItem onClick={toggleDrawer(false)}>Home</ListItem>
+            </Link>
+            <ListItem onClick={toggleServicesDropdown} className="off_service">
+              <p>Our Service</p>
+
+              {showServicesDropdown ? (
                 <LiaAngleUpSolid /> // Display up arrow if true
               ) : (
                 <LiaAngleDownSolid /> // Display down arrow if false
               )}
-              </ListItem>
-              {showServicesDropdown && (
-                <div className="Off_service_container">
-                  <ListItem className="Off_services" onClick={toggleDrawer(false)}>
-                  <Link to="/service" style={{textDecoration:"none",color:"#000"}}> <h3 style={{fontSize:"16px",fontWeight:"400"}}>Virtual construction</h3></Link>
-                    <FaAngleRight />
-                  </ListItem>
-                  <ListItem className="Off_services" onClick={toggleDrawer(false)}>
-                  <Link to="/service" style={{textDecoration:"none",color:"#000"}}><h3 style={{fontSize:"16px",fontWeight:"400"}}>Structural engineering service</h3></Link>
-                    <FaAngleRight />
-                  </ListItem>
-                  <ListItem className="Off_services" onClick={toggleDrawer(false)}>
-                  <Link to="/service" style={{textDecoration:"none",color:"#000"}}> <h3 style={{fontSize:"16px",fontWeight:"400"}}>MEP engineering service</h3></Link>
-                    <FaAngleRight />
-                  </ListItem>
-                  <ListItem className="Off_services" onClick={toggleDrawer(false)}>
-                  <Link to="/service" style={{textDecoration:"none",color:"#000"}}><h3 style={{fontSize:"16px",fontWeight:"400"}}>Support service</h3></Link>
-                    <FaAngleRight />
-                  </ListItem>
-                </div>
-              )}
-              <Link to="/about" style={{textDecoration:"none",color:"#000"}}> <ListItem onClick={toggleDrawer(false)}>About us</ListItem></Link>
-              <Link to="/contact" style={{textDecoration:"none",color:"#000"}}><ListItem onClick={toggleDrawer(false)}>Contact us</ListItem></Link>
+            </ListItem>
+            {showServicesDropdown && (
+              <div className="Off_service_container">
+                <ListItem
+                  className="Off_services"
+                  onClick={toggleDrawer(false)}
+                >
+                  <Link
+                    to="/service"
+                    style={{ textDecoration: "none", color: "#000" }}
+                  >
+                    {" "}
+                    <h3 style={{ fontSize: "16px", fontWeight: "400" }}>
+                      Virtual construction
+                    </h3>
+                  </Link>
+                  <FaAngleRight />
+                </ListItem>
+                <ListItem
+                  className="Off_services"
+                  onClick={toggleDrawer(false)}
+                >
+                  <Link
+                    to="/service"
+                    style={{ textDecoration: "none", color: "#000" }}
+                  >
+                    <h3 style={{ fontSize: "16px", fontWeight: "400" }}>
+                      Structural engineering service
+                    </h3>
+                  </Link>
+                  <FaAngleRight />
+                </ListItem>
+                <ListItem
+                  className="Off_services"
+                  onClick={toggleDrawer(false)}
+                >
+                  <Link
+                    to="/service"
+                    style={{ textDecoration: "none", color: "#000" }}
+                  >
+                    {" "}
+                    <h3 style={{ fontSize: "16px", fontWeight: "400" }}>
+                      MEP engineering service
+                    </h3>
+                  </Link>
+                  <FaAngleRight />
+                </ListItem>
+                <ListItem
+                  className="Off_services"
+                  onClick={toggleDrawer(false)}
+                >
+                  <Link
+                    to="/service"
+                    style={{ textDecoration: "none", color: "#000" }}
+                  >
+                    <h3 style={{ fontSize: "16px", fontWeight: "400" }}>
+                      Support service
+                    </h3>
+                  </Link>
+                  <FaAngleRight />
+                </ListItem>
               </div>
-            </List>
+            )}
+            <Link to="/about" style={{ textDecoration: "none", color: "#000" }}>
+              {" "}
+              <ListItem onClick={toggleDrawer(false)}>About us</ListItem>
+            </Link>
+            <Link
+              to="/contact"
+              style={{ textDecoration: "none", color: "#000" }}
+            >
+              <ListItem onClick={toggleDrawer(false)}>Contact us</ListItem>
+            </Link>
           </div>
-        </Box>
-      )
-    
+        </List>
+      </div>
+    </Box>
+  );
+
   return (
     <div>
-            <header className="header">
+      <header className="header">
         <div className="logo_sec">
           <img src={logo} alt="" />
         </div>
         <div className="nav_sec">
           <nav className="nav_link">
-            <Link to="/" style={{textDecoration:"none",color:"#000"}}><p className='links'>Home</p></Link>
-  
-          <p className='show_service'     onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave} >Our services</p>
-          {showServicesDropdown && 
-               
-        <div className="service_container">
-             <div className="services">
-           <Link to="/service" style={{textDecoration:"none",color:"#000"}}>
-            <h3 style={{fontWeight:"400",fontSize:"16px"}}>virtula construction</h3></Link>
-            <FaAngleRight />
-          </div>
-          <div className="services">
-          <Link to="/service" style={{textDecoration:"none",color:"#000"}}> <h3 style={{fontWeight:"400",fontSize:"16px"}}>structural engineering service</h3></Link>
-            <FaAngleRight />
-          </div>
-          <div className="services">
-          <Link to="/service" style={{textDecoration:"none",color:"#000"}}> <h3 style={{fontWeight:"400",fontSize:"16px"}}>MEP engineering service</h3></Link>
-            <FaAngleRight />
-          </div>
-          <div className="services">
-          <Link to="/service" style={{textDecoration:"none",color:"#000"}}><h3 style={{fontWeight:"400",fontSize:"16px"}}>support service</h3></Link>
-            <FaAngleRight />
-          </div>
-        </div>
-          }
+            <Link to="/" style={{ textDecoration: "none", color: "#000" }}>
+              <p className="links">Home</p>
+            </Link>
+            <div
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              <p>Our services</p>
+              {showBox && (
+                <div className="service_container">
+                  <div className="services">
+                    <Link
+                      to="/service"
+                      style={{ textDecoration: "none", color: "#000" }}
+                    >
+                      <h3 style={{ fontWeight: "400", fontSize: "16px" }}>
+                        virtula construction
+                      </h3>
+                    </Link>
+                    <FaAngleRight />
+                  </div>
+                  <div className="services">
+                    <Link
+                      to="/service"
+                      style={{ textDecoration: "none", color: "#000" }}
+                    >
+                      {" "}
+                      <h3 style={{ fontWeight: "400", fontSize: "16px" }}>
+                        structural engineering service
+                      </h3>
+                    </Link>
+                    <FaAngleRight />
+                  </div>
+                  <div className="services">
+                    <Link
+                      to="/service"
+                      style={{ textDecoration: "none", color: "#000" }}
+                    >
+                      {" "}
+                      <h3 style={{ fontWeight: "400", fontSize: "16px" }}>
+                        MEP engineering service
+                      </h3>
+                    </Link>
+                    <FaAngleRight />
+                  </div>
+                  <div className="services">
+                    <Link
+                      to="/service"
+                      style={{ textDecoration: "none", color: "#000" }}
+                    >
+                      <h3 style={{ fontWeight: "400", fontSize: "16px" }}>
+                        support service
+                      </h3>
+                    </Link>
+                    <FaAngleRight />
+                  </div>
+                </div>
+              )}
+            </div>
 
-          <Link to="/about" style={{textDecoration:"none",color:"#000"}}> <p>About us</p></Link>
-          <Link to="/contact" style={{textDecoration:"none",color:"#000"}}> <p>Contact us</p></Link>
+            <Link to="/about" style={{ textDecoration: "none", color: "#000" }}>
+              {" "}
+              <p>About us</p>
+            </Link>
+            <Link
+              to="/contact"
+              style={{ textDecoration: "none", color: "#000" }}
+            >
+              {" "}
+              <p>Contact us</p>
+            </Link>
           </nav>
-          <a href="mailto:email@example.com?subject=Hello&body=I'd like to discuss...">  <button>get quote now</button>
-</a>
+          <a href="mailto:email@example.com?subject=Hello&body=I'd like to discuss...">
+            {" "}
+            <button>get quote now</button>
+          </a>
         </div>
         <div className="toggle_icon">
-        <img src={menu} alt="" onClick={toggleDrawer(true)} />
-      </div>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        {drawerList}
-      </Drawer>
+          <img src={menu} alt="" onClick={toggleDrawer(true)} />
+        </div>
+        <Drawer open={open} onClose={toggleDrawer(false)}>
+          {drawerList}
+        </Drawer>
       </header>
 
-   
       <div className="banner">
         {/* <img src={banner} className="banner_img" alt="" /> */}
         {/* <img src={landing} className="res_banner_img" alt="" /> */}
@@ -138,20 +240,20 @@ export default function Header() {
               simply dummy text
             </h3>
             <p>
-              Integer in est elit. Mauris placerat blandit massa quis 
-              aliquam. Donec efficitur risus id sapien lobortis, vel
-              tempor ligula mollis. Aenean vitae nibh nec lacus  molestie
-              lobortis.
+              Integer in est elit. Mauris placerat blandit massa quis aliquam.
+              Donec efficitur risus id sapien lobortis, vel tempor ligula
+              mollis. Aenean vitae nibh nec lacus molestie lobortis.
             </p>
           </div>
 
-          <a href="mailto:email@example.com?subject=Hello&body=I'd like to discuss...">  <button>get quote now</button>
-</a>
+          <a href="mailto:email@example.com?subject=Hello&body=I'd like to discuss...">
+            {" "}
+            <button>get quote now</button>
+          </a>
         </div>
       </div>
 
       <div className="res_banner">
-
         <div className="content">
           <div className="content_text">
             <h3>
@@ -159,17 +261,21 @@ export default function Header() {
               simply dummy text
             </h3>
             <p>
-              Integer in est elit. Mauris placerat blandit massa quis 
-              aliquam. Donec efficitur risus id sapien lobortis, vel
-              tempor ligula mollis. Aenean vitae nibh nec lacus  molestie
-              lobortis.
+              Integer in est elit. Mauris placerat blandit massa quis aliquam.
+              Donec efficitur risus id sapien lobortis, vel tempor ligula
+              mollis. Aenean vitae nibh nec lacus molestie lobortis.
             </p>
           </div>
 
-          <a href="mailto:email@example.com?subject=Hello&body=I'd like to discuss..." className='a_link'>  <button>get quote now</button>
-</a>
+          <a
+            href="mailto:email@example.com?subject=Hello&body=I'd like to discuss..."
+            className="a_link"
+          >
+            {" "}
+            <button>get quote now</button>
+          </a>
         </div>
       </div>
     </div>
-  )
+  );
 }
